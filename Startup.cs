526099f888c44455b8f32cca5b0b317e2f9fb571
unknown <bruno.virginio23@gmail.com>
2019-09-22 12:53:50 -0300

    First Commit

---
+++ Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CmdApi.Models;
+
+namespace CmdApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CommandContext>
+                (opt => opt.UseSqlServer(Configuration["Data:CommandApiConnection:ConnectionString"]));
+                
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(option => option.AllowAnyOrigin());;
+            app.UseMvc();
+        }
+    }
+}
